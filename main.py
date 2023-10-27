11045656fd4d05a11560ede3ff4469551a6e40fe
Albert Margaryan <66197687+AlbertMargaryan@users.noreply.github.com>
2023-10-27 15:04:15 -0400

    Triangle frame

--- main.py
+++ main.py
@@ -36,9 +36,9 @@ def dimensions():
         height=6,
         activebackground="#EEEEEE",
         bd=5,
-        bg = "#FFFFFF",
-        font = (fontName, 25, "bold"),
-        command = lambda:chooseShape(2)
+        bg="#FFFFFF",
+        font=(fontName, 25, "bold"),
+        command=lambda: chooseShape(2)
     )
     threeDButton = tk.Button(
         text="3D",
@@ -55,7 +55,78 @@ def dimensions():
 
 
 def chooseShape(n):
-    print(n)
+    if n==2:
+        for widget in window.winfo_children():
+            widget.destroy()
+
+        window.columnconfigure(0, weight=1)  # Center-align the first column
+        window.columnconfigure(1, weight=1)
+        window.rowconfigure(0, weight=1)
+        window.rowconfigure(1, weight=3)
+        window.rowconfigure(2, weight=3)
+
+        chooseShape = tk.Label(
+            text="Choose the shape:",
+            font=(fontName, 25)
+        )
+        chooseShape.grid(column=0, columnspan=2, row=0, padx=5, pady=5)
+
+        frameTriangles = Frame(window, width=500, height=300, bg='#DDDDDD')
+        frameTriangles.grid(column=0, row=1, padx=5, pady=5)
+
+        frameTriangles.columnconfigure(0, weight=1)  # Center-align the first column
+        frameTriangles.columnconfigure(1, weight=1)
+        frameTriangles.rowconfigure(0, weight=1)
+        frameTriangles.rowconfigure(1, weight=1)
+
+        triangleButton = tk.Button(frameTriangles,
+            text="Triangle",
+            activebackground="#EEEEEE",
+            bd=5,
+            bg="#FFFFFF",
+            font=(fontName, 20, "bold"),
+
+        )
+        rightTriangleButton = tk.Button(frameTriangles,
+            text="Right Triangle",
+            activebackground="#EEEEEE",
+            bd=5,
+            bg="#FFFFFF",
+            font=(fontName, 20, "bold"),
+
+        )
+        isoTriangleButton = tk.Button(frameTriangles,
+            text="Isosceles Triangle",
+            activebackground="#EEEEEE",
+            bd=5,
+            bg="#FFFFFF",
+            font=(fontName, 20, "bold"),
+
+        )
+        equaTriangleButton = tk.Button(frameTriangles,
+            text="Equilateral Triangle",
+            activebackground="#EEEEEE",
+            bd=5,
+            bg="#FFFFFF",
+            font=(fontName, 20, "bold"),
+
+        )
+
+        triangleButton.grid(column=0, row=0, padx=5, pady=5)
+        rightTriangleButton.grid(column=1, row=0, padx=5, pady=5)
+        isoTriangleButton.grid(column=0, row=1, padx=5, pady=5)
+        equaTriangleButton.grid(column=1, row=1, padx=5, pady=5)
+
+
+
+        frameQuad = Frame(window, width=500, height=600, bg='#DDDDDD')
+        frameQuad.grid(column=1, row=1, rowspan=2, padx=5, pady=5)
+
+        frameCircles = Frame(window, width=500, height=300, bg='#DDDDDD')
+        frameCircles.grid(column=0, row=2, padx=5, pady=5)
+
+    elif n==3:
+        print (n)
 
 
 dimensions()
